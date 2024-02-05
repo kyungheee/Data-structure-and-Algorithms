1ca67c789a6e3d50ff03e3b24d9b2e0f7bd4ffef
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2024-02-05 13:39:38 +0900

    feat : 배열_제어하기.py

---
+++ day-01/devzoe/배열_제어하기.py
@@ -0,0 +1,46 @@
+Python 3.6.5 (v3.6.5:f59c0932b4, Mar 28 2018, 05:52:31) 
+[GCC 4.2.1 Compatible Apple LLVM 6.0 (clang-600.0.57)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> ls
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    ls
+NameError: name 'ls' is not defined
+>>> solution()
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    solution()
+NameError: name 'solution' is not defined
+>>> 
+===== RESTART: /Users/rudal/UpSing Dropbox/체다치즈/Mac/Documents/new.py =====
+>>> 
+===== RESTART: /Users/rudal/UpSing Dropbox/체다치즈/Mac/Documents/new.py =====
+찾기
+>>> 
+===== RESTART: /Users/rudal/UpSing Dropbox/체다치즈/Mac/Documents/new.py =====
+>>> 
+===== RESTART: /Users/rudal/UpSing Dropbox/체다치즈/Mac/Documents/new.py =====
+[2, 2, 3, 44, 90]
+>>> 
+ RESTART: /Users/rudal/UpSing Dropbox/체다치즈/Mac/Documents/배열_제어하기.py 
+Traceback (most recent call last):
+  File "/Users/rudal/UpSing Dropbox/체다치즈/Mac/Documents/배열_제어하기.py", line 6, in <module>
+    print(solution([1,1,1,999,999,3,32,2,2]))
+  File "/Users/rudal/UpSing Dropbox/체다치즈/Mac/Documents/배열_제어하기.py", line 3, in solution
+    a.sort()
+AttributeError: 'set' object has no attribute 'sort'
+>>> 
+ RESTART: /Users/rudal/UpSing Dropbox/체다치즈/Mac/Documents/배열_제어하기.py 
+Traceback (most recent call last):
+  File "/Users/rudal/UpSing Dropbox/체다치즈/Mac/Documents/배열_제어하기.py", line 6, in <module>
+    print(solution([1,1,1,999,999,3,32,2,2]))
+  File "/Users/rudal/UpSing Dropbox/체다치즈/Mac/Documents/배열_제어하기.py", line 3, in solution
+    sort(a)
+NameError: name 'sort' is not defined
+>>> 
+ RESTART: /Users/rudal/UpSing Dropbox/체다치즈/Mac/Documents/배열_제어하기.py 
+[1, 2, 3, 32, 999]
+>>> 
+ RESTART: /Users/rudal/UpSing Dropbox/체다치즈/Mac/Documents/배열_제어하기.py 
+[999, 32, 3, 2, 1]
+>>> 

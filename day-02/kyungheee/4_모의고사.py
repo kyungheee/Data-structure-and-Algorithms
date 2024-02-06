87ddf04302eeda9435ba25fa2ed1d33448317cad
경희 <148427964+kyungheee@users.noreply.github.com>
2024-02-07 00:32:29 +0900

    update

--- day-02/kyungheee/4_모의고사.py
+++ day-02/kyungheee/4_모의고사.py
@@ -5,15 +5,20 @@ def solution(answers):
         [2,1,2,3,2,4,2,5],
         [3,3,1,1,2,2,4,4,5,5]
     ]
-    
+
+    # [0,0,0] 이랑 같은 표현
     scores = [0]*3
 
+    #패턴이랑 정답이 얼마나 일치하는지
     for i , answer in enumerate(answers):
         for j, pattern in enumerate(patterns):
             if answer == pattern[i%len(pattern)]:
                 scores += 1
+                
+    # 가장 높은 점수 저장            
     max_score = max(scores)
 
+    # 가장 높은 점수를 가진 사람이 한 명이 아닐 수도 있다!
     highest_scores = []
     for i, score in enumerate(scores):
         if score == max_score:

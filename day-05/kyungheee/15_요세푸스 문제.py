c2b6fe981ef32b750b6bc5912328b4bd76350b43
경희 <148427964+kyungheee@users.noreply.github.com>
2024-02-09 02:22:31 +0900

    update

--- day-05/kyungheee/15_요세푸스 문제.py
+++ day-05/kyungheee/15_요세푸스 문제.py
@@ -6,7 +6,7 @@ def solution(N, k):
     queue = deque(range(1,N+1))
 
     while len(queue)>1: # deque에 하나의 요소가 남을 때까지
-        for _ in range(K-1):
+        for _ in range(k-1):
             queue.append(queue.popleft()) # k번째 요소를 찾기 위해 앞에서부터 제거하고 뒤에 추가
             queue.popleft() # K번째 요소 제거
     return queue[0] # 마지막으로 남은 요소 반환

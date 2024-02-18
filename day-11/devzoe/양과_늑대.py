2575817e336d900741708d96f329ae233d910d7c
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2024-02-18 14:41:59 +0900

    feat : 양과_늑대.py

---
+++ day-11/devzoe/양과_늑대.py
@@ -0,0 +1,23 @@
+def solution(info, edges):
+    visited = [0] * len(info)
+    answer = []
+    
+    def dfs(sheep, wolf):
+        if sheep > wolf:
+            answer.append(sheep)
+        else:
+            return 
+        for parent, child in edges:
+            if visited[parent] and not visited[child]:
+                visited[child] = 1
+                if info[child] == 0:
+                    dfs(sheep+1, wolf)
+                else:
+                    dfs(sheep, wolf+1)
+                visited[child] = 0
+
+    visited[0] = 1
+    dfs(1, 0)
+    return max(answer)
+
+
